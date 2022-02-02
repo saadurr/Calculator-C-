c200b26c508125bd48fb44d2605d5c607697224b
Saad Ur Rehman <saad.ur.rehman77@gmail.com>
2022-02-02 18:35:27 +0500

    Adds functionality to calculator

--- CLI/basic-calculator/basic-calculator/basic-calculator.cpp
+++ CLI/basic-calculator/basic-calculator/basic-calculator.cpp
@@ -6,9 +6,29 @@
 int main()
 {
     /*All variables to be used in this program*/
-    char input = ' ';
-    char inparr[7] = { 'a', 's', 'm', 'd', 'l', 'o', 'q' };
-    int validinp = -1;
+    char input = ' ', sign = ' ';
+    char inparr[7] = { 'a', 's', 'm', 'd', 'e', 'x', 'q' };
+    map <char, string> operationsNameMap = {
+        {'a', "Addition"},
+        {'s', "Subtraction"},
+        {'m', "Multiplcation"},
+        {'d', "Division"},
+        {'e', "Exponent"},
+        {'x', "Scientific Menu"},
+        {'q', "Quit"}
+    };
+    map <char, char> operationsSignMap = {
+        {'a', '+'},
+        {'s', '-'},
+        {'m', 'x'},
+        {'d', '/'},
+        {'e', '^'}
+    };
+    int validinp = -1, sizeArr = -1;
+    double val1 = -9999, val2 = -9999, output = -9999;
+    string subMenu = "";
+
+    sizeArr = sizeof(inparr) / sizeof(inparr[0]);
 
     printStartWindow();
     clearScreen();
@@ -19,10 +39,27 @@ int main()
         cin >> input;
         input = tolower(input);
 
-        validinp = validateInput(input, inparr);
-        //this function shall return an integer based on whatever was entered
+        validinp = validateInput(input, inparr, sizeArr);
         
+        if (!validinp)
+        {
+            cout << "Wrong Input!" << endl;
+            waitForKey();
+            continue;
+        }
+        else if (input == 'q')
+        {
+            quitApplication();
+            break;
+        }
 
+        subMenu = operationsNameMap[input];
+        sign = operationsSignMap[input];
+        preprocessInput(val1, val2, input, subMenu, sign);
+
+        output = calculateEngine(input, val1, val2);
+
+        displayOutput(subMenu, sign, output, val1, val2);
         
     }
     return 0;
--- CLI/basic-calculator/basic-calculator/calculator.h
+++ CLI/basic-calculator/basic-calculator/calculator.h
@@ -3,14 +3,19 @@
 #include <stdlib.h>
 #include <string>
 #include <cctype>
+#include <map>
 
 using namespace std;
 /*All functions to be used in this program*/
 
-void clearScreen()
+void waitForKey()
 {
     cout << "\n\n\n\t\tPress any key to continue..." << endl;
     cin.get();
+}
+void clearScreen()
+{
+    waitForKey();
     system("CLS");
 }
 
@@ -21,12 +26,119 @@ void printStartWindow()
 
 void printMenu()
 {
-    cout << "Menu" << endl;
+    system("CLS");
+
+    cout << "\n\t\tCalculator App" << endl;
+    cout << "\n\t\tMain Menu" << endl;
+    cout << "\n\n\tSelect any of the operation below:" << endl;
+    cout << "\ta. Addition" << endl;
+    cout << "\ts. Subtraction" << endl;
+    cout << "\tm. Multiplication" << endl;
+    cout << "\td. Division" << endl;
+    cout << "\te. Exponent" << endl;
+    cout << "\tx. Scientific Menu" << endl;
+    cout << "\n\tq. Quit" << endl;
 }
 
-int validateInput(char testch, char chararr[])
+void printSubmenu(string menuName, char sign)
+{
+    system("CLS");
+    cout << "\n\t\tCalculator App" << endl;
+    cout << "\n\n\tYou have selected " << menuName << "." << endl;
+    cout << "\n\n\t\tnum1 " << sign << " num2" << endl;
+    cout << "\tPlease Enter First Operand for " << menuName << ":" << endl;
+}
+
+int validateInput(char testch, char chararr[], int sizeArr)
 {
 
+    for (int i = 0; i < sizeArr; i++)
+    {
+        if (testch == chararr[i])
+        {
+            return true;
+        }
+    }
     return false;
 
+}
+
+void quitApplication()
+{
+    system("CLS");
+    cout << "\n\t\tQuitting Calculator App" << endl;
+    cout << "\n\n\n\t\tPress any key to continue..." << endl;
+    cin.get();
+}
+
+void preprocessInput(double& val1, double& val2, char inp, string menu, char sign)
+{
+
+    printSubmenu(menu, sign);
+
+    cin >> val1;
+
+    cout << "\n\tEnter second operand for " << menu << ":" << endl;
+    
+    cin >> val2;
+
+
+
+}
+
+
+void displayOutput(string menuName, char sign, double output, double val1, double val2)
+{
+    clearScreen();
+
+    cout << "\n\t\t Calculator App - Output Screen for " << menuName << " Result" << endl;
+
+    cout << "\n\n\t " << val1 << " " << sign << " " << val2 << " = " << output << endl;
+
+    waitForKey();
+}
+
+template <typename T>
+void addition(T val1, T val2, T& result)
+{
+    result = val1 + val2;
+}
+
+template <typename T>
+void subtraction(T val1, T val2, T& result)
+{
+    result = val1 - val2;
+}
+
+template <typename T>
+void multiplication(T val1, T val2, T& result)
+{
+    result = val1 * val2;
+}
+
+template <typename T>
+void division(T val1, T val2, T& result)
+{
+    result = val1 / val2;
+}
+
+template <typename T>
+void exponent(T val1, T val2, T& result)
+{
+    result = pow(val1, val2);
+}
+
+
+template <typename T>
+T calculateEngine(char operation, T val1, T val2)
+{
+    T result = 0.00;
+
+    if (operation == 'a') addition(val1, val2, result);
+    else if (operation == 's') subtraction(val1, val2, result);
+    else if (operation == 'm') multiplication(val1, val2, result);
+    else if (operation == 'd') division(val1, val2, result);
+    else if (operation == 'e') exponent(val1, val2, result);
+
+    return result;
 }
